f270bee496a8cc433cef3ad980dbde5c87d251ea
shironetsu <70092342+shironetsu@users.noreply.github.com>
2023-01-19 22:13:47 +0900

    Implement sorting by strokes

---
+++ chinese-character-stroke-order/src/chinese_character/mod.rs
@@ -0,0 +1,64 @@
+pub mod stroke_table;
+use stroke_table::STROKE_TABLE;
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug, Ord)]
+pub struct ChineseCharacter {
+    value: char,
+}
+
+impl ChineseCharacter {
+    pub fn from(c: char) -> Result<Self, StrokeIsUnknownError> {
+        if STROKE_TABLE.contains_key(&c) {
+            Ok(ChineseCharacter { value: c })
+        } else {
+            Err(StrokeIsUnknownError)
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn from_unchecked(c: char) -> Self {
+        Self::from(c).unwrap()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct StrokeIsUnknownError;
+
+impl PartialOrd for ChineseCharacter {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let a = STROKE_TABLE.get(&self.value).unwrap();
+        let b = STROKE_TABLE.get(&other.value).unwrap();
+        Some(a.len().cmp(&b.len()).then_with(|| a.cmp(&b)))
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Debug, PartialOrd, Ord)]
+pub struct ChineseString {
+    chars: Vec<ChineseCharacter>,
+}
+
+impl ChineseString {
+    pub fn from(s: String) -> Result<Self, ContainsUnexpectedCharError> {
+        let mut chars = vec![];
+        for (i, c) in s.chars().enumerate() {
+            let res = ChineseCharacter::from(c);
+            if res.is_ok() {
+                chars.push(res.unwrap());
+            } else {
+                return Err(ContainsUnexpectedCharError {
+                    position: i,
+                    character: c,
+                });
+            }
+        }
+        Ok(ChineseString { chars })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ContainsUnexpectedCharError {
+    #[allow(dead_code)]
+    position: usize,
+    #[allow(dead_code)]
+    character: char,
+}
